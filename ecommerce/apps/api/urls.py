7b8441a22814c51c77c8e5d6bebcb8b1f65b9728
Getasy <getasy.in@gmail.com>
2025-12-20 17:50:51 +0530

    refactor views and urls to use class-based views for improved structure and maintainability

--- ecommerce/apps/api/urls.py
+++ ecommerce/apps/api/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list),
-    path("products/info/", views.product_info),
-    path("products/<int:pk>/", views.product_detail),
-    path("orders/", views.order_list),
+    # On using Classed Based Views
+    path("products/", views.ProductListApiView.as_view()),
+    path("products/info/", views.ProductInfoListApiView.as_view()),
+    path("products/<int:pk>/", views.ProductDetailApiView.as_view()),
+    path("orders/", views.OrderListApiView.as_view()),
+    
+    # On using Function Based Views
+    # path("products/", views.product_list),
+    # path("products/info/", views.product_info),
+    # path("products/<int:pk>/", views.product_detail),
+    # path("orders/", views.order_list),
 ]
--- ecommerce/apps/api/views.py
+++ ecommerce/apps/api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Max
+from silk.profiling.profiler import silk_profile
 
 from apps.api.serializers import (
     ProductSerializer,
@@ -10,37 +11,78 @@ from apps.api.serializers import (
 from apps.api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+
+# Classed based Views
 
 
-@api_view(["GET"])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)  # --> It is from the (Browsable Api)
+class ProductListApiView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = [IsAdminUser]
 
 
-@api_view(["GET"])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailApiView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
-@api_view(["GET"])
-def order_list(request):
-    orders = Order.objects.all()
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)  # --> It is from the (Browsable Api)
-
-
-@api_view(["GET"])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer(
-        {
-            "products   ": products,
-            "count": len(products),
-            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
-        }
+class OrderListApiView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related(
+        "items__product",
     )
-    return Response(serializer.data)
+    serializer_class = OrderSerializer
+
+
+class ProductInfoListApiView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer(
+            {
+                "products": products,
+                "count": len(products),
+                "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+            }
+        )
+        return Response(serializer.data)
+
+
+# Function Based Views
+
+# @api_view(["GET"])
+# def product_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)  # --> It is from the (Browsable Api)
+
+
+# @api_view(["GET"])
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+
+# @silk_profile(name="order_list_view")
+# @api_view(["GET"])
+# def order_list(request):
+#     orders = Order.objects.prefetch_related(
+#         "items__product",
+#     )
+#     serializer = OrderSerializer(orders, many=True)
+#     return Response(serializer.data)  # --> It is from the (Browsable Api)
+
+
+# @api_view(["GET"])
+# def product_info(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer(
+#         {
+#             "products   ": products,
+#             "count": len(products),
+#             "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+#         }
+#     )
+#     return Response(serializer.data)
