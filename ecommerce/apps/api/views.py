871d7e14095544ea1ad5a25defc1a4906fd8c448
Getasy <getasy.in@gmail.com>
2025-12-20 16:43:40 +0530

    add product_info view and order_list view; update urls for new endpoints

--- ecommerce/apps/api/views.py
+++ ecommerce/apps/api/views.py
@@ -1,23 +1,46 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.db.models import Max
 
-from apps.api.serializers import ProductSerializer
-from apps.api.models import Product
+from apps.api.serializers import (
+    ProductSerializer,
+    OrderSerializer,
+    ProductInfoSerializer,
+)
+from apps.api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def product_list(request):
-   products = Product.objects.all()
-   serializer = ProductSerializer(products, many=True)
-   return Response(serializer.data)  # --> It is from the (Browsable Api)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)  # --> It is from the (Browsable Api)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def product_detail(request, pk):
-   product = get_object_or_404(Product, pk=pk)
-   serializer = ProductSerializer(product)
-   return Response(serializer.data)
- 
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
 
+@api_view(["GET"])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)  # --> It is from the (Browsable Api)
+
+
+@api_view(["GET"])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer(
+        {
+            "products   ": products,
+            "count": len(products),
+            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+        }
+    )
+    return Response(serializer.data)
