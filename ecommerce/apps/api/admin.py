0f05f58b497eeaead0f0a1ba6ed4b42b1517f1ef
Getasy <getasy.in@gmail.com>
2025-12-22 17:25:34 +0530

    register Order and OrderItem models in admin interface

--- ecommerce/apps/api/admin.py
+++ ecommerce/apps/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInLine]
+
+
+admin.site.register(Order, OrderAdmin)
