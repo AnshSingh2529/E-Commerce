d311f86676a5359ae397b0968f6640188d8c7ffb
Getasy <getasy.in@gmail.com>
2025-12-24 12:32:47 +0530

    update API URLs to use ProductRetrieveUpdateDestroyApiView and add JWT token endpoints

--- ecommerce/apps/api/urls.py
+++ ecommerce/apps/api/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path("products/", views.ProductListCreateApiView.as_view()),
     # path("products/create/", views.ProductCreateApiView.as_view()),
     path("products/info/", views.ProductInfoListApiView.as_view()),
-    path("products/<int:pk>/", views.ProductDetailApiView.as_view()),
+    path(
+        "products/<int:product_id>/",
+        views.ProductRetrieveUpdateDestroyApiView.as_view(),
+    ),
     path("orders/", views.OrderListApiView.as_view()),
     path("user-orders/", views.UserOrderListApiView.as_view(), name="user-orders"),
     # On using Function Based Views
